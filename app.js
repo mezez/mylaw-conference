ea73db31c01e0df29598b2ce09ff5da1f553bddb
mezez <ekemammezez@gmail.com>
2019-12-06 02:27:27 +0100

    modification for server port

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
     //mongoose.connect('mongodb://localhost/talk', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(result => {
 
-        const server = app.listen(8080);
+        const server = app.listen(process.env.PORT || 3000);
 
         //socketio connection
         const io = require('./socket').init(server);
